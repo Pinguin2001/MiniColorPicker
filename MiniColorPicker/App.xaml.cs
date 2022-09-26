57c0f82d52322e94ee7c2372867ea812060e7735
JULIΛN (Pinguin2001) <75797743+Pinguin2001@users.noreply.github.com>
2022-09-26 18:09:43 +0200

    Upload v1.0 source code

---
+++ MiniColorPicker/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace MiniColorPicker
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ MiniColorPicker/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    [System.STAThreadAttribute()]
+    public static void Main()
+    {
+        using (new UI.App())
+        {
+            MiniColorPicker.App app = new MiniColorPicker.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
